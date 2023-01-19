d2861cc6f857ba498c375e546579227d0a120f17
plitche <kysu728@naver.com>
2023-01-19 22:56:27 +0900

    13. Destructuring - Destructuring Array

---
+++ Project/13.DestructuringArray.js
@@ -0,0 +1,8 @@
+// Destructuring
+let data = ['crong', 'honux', 'jk', 'jinny']
+let jisu = data[0]
+let jung = data[2]
+console.log(jisu, jung)
+
+let [jisu1,,jung2] = data;
+console.log(jisu1, jung2)
