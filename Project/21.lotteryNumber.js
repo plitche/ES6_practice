912e64510ca7d3f4bd81312e5376346496d20739
plitche <kysu728@naver.com>
2023-01-25 23:41:41 +0900

    21. 로또 번호 생성기

---
+++ Project/21.lotteryNumber.js
@@ -0,0 +1,22 @@
+// Destructuring and set 활용
+// 1. 유일한 값을 추출하는 과정에서 Set을 사용
+// 2. getRandomNumber함수에 변수를 전달하는 과정에서 destructing을 사용
+
+const SETTING = {
+    name: 'LUCKY LOTTO!',
+    count : 6,
+    maxNumber : 45
+}
+
+let numbers = [];
+
+
+function getRandomNumber(maxNumber) {
+
+}
+
+for (let i=0; i<SETTING.count; i++) {
+    getRandomNumber(maxNumber);
+}
+
+console.log(colorSet.value();)
