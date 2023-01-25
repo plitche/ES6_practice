12b85bb29e04cdf965f53504691e5450ae8621db
plitche <kysu728@naver.com>
2023-01-25 23:49:24 +0900

    풀이 : Destructuring을 어떻게 쓰지 ?

--- Project/21.lotteryNumber.js
+++ Project/21.lotteryNumber.js
@@ -2,21 +2,35 @@
 // 1. 유일한 값을 추출하는 과정에서 Set을 사용
 // 2. getRandomNumber함수에 변수를 전달하는 과정에서 destructing을 사용
 
+
 const SETTING = {
     name: 'LUCKY LOTTO!',
     count : 6,
     maxNumber : 45
 }
 
-let numbers = [];
-
+/*
+let numbers = new Set;
 
 function getRandomNumber(maxNumber) {
-
+    numbers.add(Math.round(Math.random() * maxNumber)) + 1 
 }
 
 for (let i=0; i<SETTING.count; i++) {
-    getRandomNumber(maxNumber);
+    getRandomNumber(SETTING.maxNumber);
 }
 
-console.log(colorSet.value();)
+console.log(colorSet.value())
+*/
+
+let numbers = new Set;
+
+function getRandomNumber(maxNumber) {
+    numbers.add(Math.round(Math.random() * maxNumber)) + 1 
+}
+
+for (let i=0; i<SETTING.count; i++) {
+    getRandomNumber(SETTING.maxNumber);
+}
+
+console.log(colorSet.value())
